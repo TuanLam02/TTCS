149696e1fe0203f621004b5c5454bcf679795e29
Ngo Tuan Lam <58735659+TuanLam02@users.noreply.github.com>
2022-12-09 21:30:54 +0700

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -9,11 +9,6 @@ void docfile(){
 	fstream inp("input.txt");
 	inp>>n>>m;
 	inp>>s>>e;
-	for(int i = 1;i<=n; i++){
-		for(int j = 1; j<=n; j++){
-			a[i][j] = -1;
-		}
-	}
 	for(int i=1; i<=m; i++){
 		int u,v;
 		inp>>u>>v;
@@ -26,11 +21,10 @@ void dijkstra(){
 	for(int i = 1; i <= n; i++){
 		f[i]=maxf;
 		checkdinh[i]=true;
-		
 	}
 	f[s]=0;
 	int v=s, fmin;
-	while(v!=e){// dinh dau khac dinh cuoi
+	while(v!=e){
 		fmin=maxf;
 		for(int i = 1; i <= n; i++){
 			if(checkdinh[i] && fmin>f[i]){
