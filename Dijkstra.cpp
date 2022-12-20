d5b0c8b7423421eec1785f43dd835fe666c31b8a
Ngo Tuan Lam <58735659+TuanLam02@users.noreply.github.com>
2022-12-20 18:01:06 +0700

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,15 +1,20 @@
-#include <iostream>
-#include <fstream>
-#define maxn 1000
-#define maxf 1000
+#include<iostream>
+#include<fstream>
+#define maxn 100
+#define maxf 10000000
 using namespace std;
 int a[maxn][maxn],n,m,s,e;
-int f[maxn],vet[maxn],checkdinh[maxn];
+int f[maxn],vet[maxn],check[maxn];
 void docfile(){
 	fstream inp("input.txt");
 	inp>>n>>m;
-	inp>>s>>e;
-	for(int i=1; i<=m; i++){
+	inp>>s;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			a[i][j]=maxf;
+		}
+	}
+	for(int i=1;i<=m;i++){
 		int u,v;
 		inp>>u>>v;
 		inp>>a[u][v];
@@ -18,51 +23,66 @@ void docfile(){
 	inp.close();
 }
 void dijkstra(){
-	for(int i = 1; i <= n; i++){
+	for(int i=1;i<=n;i++){
+		check[i]=true;
 		f[i]=maxf;
-		checkdinh[i]=true;
 	}
-	f[s]=0;
-	int v=s, fmin;
-	while(v!=e){
-		fmin=maxf;
-		for(int i = 1; i <= n; i++){
-			if(checkdinh[i] && fmin>f[i]){
-				fmin=f[i];
-				v=i;
+	vet[maxn]=s;
+	int v=s, minf;
+	f[v]=0;
+	int k=0;
+	while(k<=n){
+		if(v!=k){
+			minf=maxf;
+			for(int j=1;j<=n;j++){
+				if(check[j]==true&&minf>f[j]){
+					minf=f[j];
+					v=j;
+				}
+			}
+			check[k]=false;
+			check[v]=false;
+			for(int j=1;j<=n;j++){
+				if(a[v][j]!=maxf&&f[j]>f[v]+a[v][j]){
+					f[j]=f[v]+a[v][j];
+					vet[j]=v;
+				}
 			}
 		}
-		if(fmin==maxf){
-		break;			
-		}
-		checkdinh[v]=false;
-		for(int i = 1; i<=n;i++){
-			if(a[v][i]>0 && f[i]>f[v]+a[v][i]){
-				f[i]=f[v]+a[v][i];
-				vet[i]=v;
-			}
-		}
+		k++;
+		
 	}
 }
-void ketqua(){
-	if(f[e]==maxf) cout<<"khong co duong di";
-	else{
-		cout<<f[e]<<endl;
-		int duong[maxn],d=0;
-		d++;
-		duong[d]=e;
-		while(vet[e]!=0){
-			e=vet[e];
+void output(){
+	
+	for(int i=1;i<=n;i++){
+		if(s==i){
+			cout<<"Khong co duong di den: "<<i<<endl;
+			cout<<endl;
+		} 
+		else{
+			cout<<"Trong so di den "<<i<<" la: "<<f[i]<<endl;
+			cout<<"Duong di la: ";
+			int duong[maxn], d=0;
 			d++;
-			duong[d]=e;
+			int k=i;
+			duong[d]=k;
+			while(vet[k]!=0){
+				k=vet[k];
+				d++;
+				duong[d]=k;
+			}
+			for(int j=d;j>0;j--){
+			cout<<duong[j]<<" ";
+			}
+			cout<<endl;
+			cout<<endl;
 		}
-		for(int i = d;i>0;i--){
-			cout<<duong[i]<<" ";
-		}
-	}
+		
+	}	
 }
-main(){
+int main(){
 	docfile();
 	dijkstra();
-	ketqua();
+	output();
 }
