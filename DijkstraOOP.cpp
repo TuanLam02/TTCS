8af0bcd0a704b14290b8064c1f051bdfd5ce0335
Ngo Tuan Lam <58735659+TuanLam02@users.noreply.github.com>
2022-12-22 17:07:34 +0700

    Update DijkstraOOP.cpp

--- DijkstraOOP.cpp
+++ DijkstraOOP.cpp
@@ -1,54 +1,55 @@
-#include<iostream>
-#include<fstream>
+#include <fstream>
+#include <iostream>
 #define maxn 100
-#define maxf 10000000
 using namespace std;
-class Duongdingannhat{
+class ShortestPath{
+	private:
+		int matrix[maxn][maxn];
+		int number_vertex;
+		int number_distance;
+		int start;
+		int weight[maxn],trace[maxn],check[maxn];
 	public:
-		int a[maxn][maxn],n,m,s;
-		int f[maxn],vet[maxn],check[maxn];
-		void Input(){
-    		freopen("input.txt", "r", stdin);
-    		freopen("output.txt", "w", stdout);
-			cin>>n>>m;
-			cin>>s;
-			for(int i=1;i<=n;i++){
-				for(int j=1;i<=n;j++){
-					a[i][j]=maxf;
+		void Create(){
+			cin>>this->number_vertex;
+			cin>>this->number_distance;
+			cin>>this->start;
+			for(int i=1;i<=this->number_vertex;i++){
+				for(int j=1;j<=this->number_vertex;j++){
+					this->matrix[i][j]=INT_MAX;
 				}
 			}
-			for(int i=1;i<=m;i++){
+			for(int i=1;i<=this->number_distance;i++){
 				int u,v;
 				cin>>u>>v;
-				cin>>a[u][v];
-				a[v][u]=a[u][v];
+				cin>>this->matrix[u][v];
+				this->matrix[v][u]=this->matrix[u][v];
 			}
-
 		}
 		void Dijkstra(){
-			for(int i=1;i<=n;i++){
+			for(int i=1;i<=this->number_vertex;i++){
 				check[i]=true;
-				f[i]=maxf;
+				weight[i]=INT_MAX;
 			}
-			vet[maxn]=s;
-			int v=s,minf;
-			f[v]=0;
+			trace[maxn]=this->start;
+			int point=this->start,fmin;
+			weight[point]=0;
 			int k=0;
-			while(k<=n){
-				minf=maxf;
-				if(v!=k){
-					for(int j=1;j<=n;j++){
-						if(check[j]==true&&minf>f[j]){
-							minf=f[j];
-							v=j;
+			while(k<=this->number_vertex){
+				if(point!=k){
+					fmin=INT_MAX;
+					for(int j=1;j<=this->number_vertex;j++){
+						if( this->check[j]==true && fmin>weight[j] ){
+							fmin=weight[j];
+							point=j;
 						}
 					}
 					check[k]=false;
-					check[v]=false;
-					for(int j=1;j<=n;j++){
-						if(a[v][j]!=maxf&&f[j]>f[v]+a[v][j]){
-							f[j]=f[v]+a[v][j];
-							vet[j]=v;
+					check[point]=false;
+					for(int j=1;j<=this->number_vertex;j++){
+						if(this->matrix[point][j]!=INT_MAX&&weight[j]>this->weight[point]+matrix[point][j]){
+							weight[j]=weight[point]+matrix[point][j];
+							trace[j]=point;
 						}
 					}
 				}
@@ -56,35 +57,39 @@ class Duongdingannhat{
 			}
 		}
 		void Output(){
-			for(int i=1;i<=n;i++){
-				if(s==i){
-					cout<<"KHONG CO DUONG DI!";
+			for(int i=1;i<=this->number_vertex;i++){
+				if(start==i){
+					cout<<"KHONG CO DUONG DI DEN: "<<i;
+					cout<<endl;
 					cout<<endl;
 				}
 				else{
-					cout<<"TRONG SO DUONG DI DEN "<<i<<" LA: "<<f[i]<<endl;
+					cout<<"TRONG SO DUONG DI DEN "<<i<<" LA: "<<weight[i]<<endl;
 					cout<<"DUONG DI DEN "<<i<<" LA: ";
-					int duong[maxn],d=0;
+					int path[maxn],d=0;
 					int k=i;
 					d++;
-					duong[d]=k;
-					while(vet[k]!=0){
-						k=vet[k];
+					path[d]=k;
+					while(trace[k]!=0){
+						k=trace[k];
 						d++;
-						duong[d]=k;
+						path[d]=k;
 					}
 					for(int j=d;j>0;j--){
-						cout<<duong[j]<<" ";
+						cout<<path[j]<<" ";
 					}
-					cout<<endl;
-					cout<<endl;
-				}
-			}
+			cout<<endl;
+			cout<<endl;
+		} 
+	}
 		}
+
 };
 int main(){
-	Duongdingannhat a;
-	a.Input();
+	ShortestPath a;
+	freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	a.Create();
 	a.Dijkstra();
 	a.Output();
 	return 0;
