dff84218159a2227538d1eb0ecc25e321821399e
Ngo Tuan Lam <58735659+TuanLam02@users.noreply.github.com>
2022-12-22 17:26:32 +0700

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,52 +1,54 @@
-#include<iostream>
-#include<fstream>
+#include <fstream>
+#include <iostream>
 #define maxn 100
-#define maxf 10000000
 using namespace std;
-int a[maxn][maxn],n,m,s,e;
-int f[maxn],vet[maxn],check[maxn];
+int matrix[maxn][maxn];
+int number_vertex;
+int number_distance;
+int start;
+int f[maxn];
+int trace[maxn];
+int check[maxn];
 void Input(){
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-	cin>>n>>m;
-	cin>>s;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			a[i][j]=maxf;
+	cin>>number_vertex>>number_distance;
+	cin>>start;
+	for(int i=1;i<=number_vertex;i++){
+		for(int j=1;j<=number_vertex;j++){
+			matrix[i][j]=INT_MAX;
 		}
 	}
-	for(int i=1;i<=m;i++){
+	for(int i=1;i<=number_distance;i++){
 		int u,v;
 		cin>>u>>v;
-		cin>>a[u][v];
-		a[v][u]=a[u][v];
+		cin>>matrix[u][v];
+		matrix[v][u]=matrix[u][v];
 	}
-
 }
 void Dijkstra(){
-	for(int i=1;i<=n;i++){
+	for(int i=1;i<=number_vertex;i++){
 		check[i]=true;
-		f[i]=maxf;
+		f[i]=INT_MAX;
 	}
-	vet[maxn]=s;
-	int v=s, minf;
-	f[v]=0;
+	trace[maxn]=start;
+	int point=start;
+	int min;
+	f[point]=0;
 	int k=0;
-	while(k<=n){
-		if(v!=k){
-			minf=maxf;
-			for(int j=1;j<=n;j++){
-				if(check[j]==true&&minf>f[j]){
-					minf=f[j];
-					v=j;
+	while(k<=number_vertex){
+		if(point!=k){
+			min=INT_MAX;
+			for(int j=1;j<=number_vertex;j++){
+				if(check[j]==true&&min>f[j]){
+					min=f[j];
+					point=j;
 				}
 			}
 			check[k]=false;
-			check[v]=false;
-			for(int j=1;j<=n;j++){
-				if(a[v][j]!=maxf&&f[j]>f[v]+a[v][j]){
-					f[j]=f[v]+a[v][j];
-					vet[j]=v;
+			check[point]=false;
+			for(int j=1;j<=number_vertex;j++){
+				if( matrix[point][j]!=INT_MAX && f[j]>f[point]+matrix[point][j]){
+					f[j]=f[point]+matrix[point][j];
+					trace[j]=point;
 				}
 			}
 		}
@@ -55,27 +57,26 @@ void Dijkstra(){
 	}
 }
 void Output(){
-	
-	for(int i=1;i<=n;i++){
-		if(s==i){
-			cout<<"KHONG CO DUONG DI!!!!!!!!!!!!";
+	for(int i=1;i<=number_vertex;i++){
+		if(start==i){
+			cout<<"KHONG CO DUONG DI DEN "<<i;
 			cout<<endl;
 			cout<<endl;
 		}
 		else{
 			cout<<"TRONG SO DUONG DI DEN "<<i<<" LA: "<<f[i]<<endl;
 			cout<<"DUONG DI DEN "<<i<<" LA: ";
-			int duong[maxn],d=0;
+			int path[maxn],d=0;
 			int k=i;
 			d++;
-			duong[d]=k;
-			while(vet[k]!=0){
-				k=vet[k];
+			path[d]=k;
+			while(trace[k]!=0){
+				k=trace[k];
 				d++;
-				duong[d]=k;
+				path[d]=k;
 			}
 			for(int j=d;j>0;j--){
-				cout<<duong[j]<<" ";
+				cout<<path[j]<<" ";
 			}
 			cout<<endl;
 			cout<<endl;
@@ -83,6 +84,8 @@ void Output(){
 	}
 }
 int main(){
+	freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 	Input();
 	Dijkstra();
 	Output();
